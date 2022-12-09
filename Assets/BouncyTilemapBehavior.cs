e4a68be62364acbaf7149bf7b26e3b02e3b7c9a5
Corey Pierce <coreypierce11@gmail.com>
2022-12-09 14:43:41 -0500

    Add function to bounce script for onStay

--- Assets/BouncyTilemapBehavior.cs
+++ Assets/BouncyTilemapBehavior.cs
@@ -44,12 +44,12 @@ public class BouncyTilemapBehavior : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            applyBounceForceOnPlayer();
+            applyBounceForceOnPlayerOnTriggerEnter();
 
         }
         else
         {
-            Debug.Log("Something hit bounce, but not player...");
+            //Debug.Log("Something hit bounce, but not player...");
         }
     }
     
@@ -58,8 +58,8 @@ public class BouncyTilemapBehavior : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("Hit bounce.");
-            applyBounceForceOnPlayer();
+            //Debug.Log("Hit bounce.");
+            applyBounceForceOnPlayerOnStay();
             //playerRb.velocity = new Vector2(playerRb.velocity.x, Mathf.Abs(playerRb.velocity.y) + bounciness);
         }
         else
@@ -68,7 +68,7 @@ public class BouncyTilemapBehavior : MonoBehaviour
         }
     }
 
-    private void applyBounceForceOnPlayer()
+    private void applyBounceForceOnPlayerOnTriggerEnter()
     {
         switch (forceDirection)
         {
@@ -99,16 +99,64 @@ public class BouncyTilemapBehavior : MonoBehaviour
                 }
                 break;
             case ForceDirection.LEFT:
-                playerRb.velocity = new Vector2(-bounciness, playerRb.velocity.y);
+                //Debug.Log("ForceDirection.LEFT");
+                //playerRb.velocity = new Vector2(-100f + playerRb.velocity.x, playerRb.velocity.y);
+                playerRb.AddForce(new Vector2(-2500f, 0f), ForceMode2D.Force);
                 break;
             case ForceDirection.RIGHT:
-                playerRb.velocity = new Vector2(bounciness, playerRb.velocity.y);
+                playerRb.velocity = new Vector2(bounciness + playerRb.velocity.x, playerRb.velocity.y);
                 break;
             default:
                 Debug.Log("DEFAULT HIT IN BouncyTilemapBehavior: " + forceDirection);
                 break;
         }
     }
-    
+
+    private void applyBounceForceOnPlayerOnStay()
+    {
+        switch (forceDirection)
+        {
+            case ForceDirection.UP:
+                /*
+                 * If the user is holding jump then increase the height of the bounce/force.
+                 */
+                if (player.isHoldingJump())
+                {
+                    playerRb.velocity = new Vector2(playerRb.velocity.x, bounciness + holdingJumpIncrease);
+                }
+                else
+                {
+                    playerRb.velocity = new Vector2(playerRb.velocity.x, bounciness);
+                }
+                break;
+            case ForceDirection.DOWN:
+                /*
+                 * If the user is already falling, then increasing negative y velocity makes it go
+                 * uncontrollably fast. So, if they're already falling, then ignore since the player
+                 * is already being forced down.
+                 */
+                if (playerRb.velocity.y < 0)
+                {
+                    playerRb.velocity = new Vector2(playerRb.velocity.x, playerRb.velocity.y);
+                }
+                else
+                {
+                    playerRb.velocity = new Vector2(playerRb.velocity.x, -bounciness + playerRb.velocity.y);
+                }
+                break;
+            case ForceDirection.LEFT:
+                //Debug.Log("ForceDirection.LEFT");
+                //playerRb.velocity = new Vector2(-100f + playerRb.velocity.x, playerRb.velocity.y);
+                playerRb.AddForce(new Vector2(-750f, 0f), ForceMode2D.Force);
+                break;
+            case ForceDirection.RIGHT:
+                playerRb.velocity = new Vector2(bounciness + playerRb.velocity.x, playerRb.velocity.y);
+                break;
+            default:
+                Debug.Log("DEFAULT HIT IN BouncyTilemapBehavior: " + forceDirection);
+                break;
+        }
+    }
+
 
 }
