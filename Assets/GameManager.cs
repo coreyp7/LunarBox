1a852ce886573c944279c12c303c6b0d92dbab0f
Corey Pierce <coreypierce11@gmail.com>
2023-01-01 15:00:15 -0500

    Get json object of all ground tiles in level

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,6 +28,8 @@ public class GameManager : MonoBehaviour
 
         TileBase[] tiles = groundTilemap.GetTilesBlock(box);
         Debug.Log("tiles.length is: "+tiles.Length);
+
+        TileList tileSerializes = new TileList();
         
         for(int x = 0; x < 71; x++)
         {
@@ -35,11 +38,52 @@ public class GameManager : MonoBehaviour
                 TileBase tile = tiles[x + y * 71]; // magic ??
                 if (tile != null)
                 {
-                    Debug.Log("Tile (" + x + "," + y + ")");
+                    //Debug.Log("Tile (" + x + "," + y + ")");
                     // Serialize Here
-
+                    //Debug.Log(JsonUtility.ToJson(new TileSerialize(x,y,"ground")));
+                    tileSerializes.tiles.Add(new TileSerialize(x, y, "ground"));
                 }
             }
+        }
+
+        foreach(TileSerialize tile in tileSerializes.tiles)
+        {
+            Debug.Log(JsonUtility.ToJson(tile));
+        }
+
+        //TileSerialize[] arr = tileSerializes.tiles.ToArray();
+        //string listToJson = JsonHelper.ToJson(arr);
+        Debug.Log(tileSerializes.tiles.Count);
+        //Debug.Log(arr.Length);
+        //Debug.Log(JsonUtility.ToJson(arr));
+        Debug.Log(JsonUtility.ToJson(tileSerializes, true));
+        //Debug.Log(JsonUtility.ToJson(tileSerializes.ToArray()));
+    }
+
+    [Serializable]
+    public class TileSerialize
+    {
+        public TileSerialize(int x, int y, string type)
+        {
+            this.x = x;
+            this.y = y;
+            this.type = type;
+        }
+
+        public int x;
+        public int y;
+        public string type;
+
+    }
+
+    [Serializable]
+    public class TileList
+    {
+        public List<TileSerialize> tiles;
+
+        public TileList()
+        {
+            this.tiles = new List<TileSerialize>();
         }
     }
 }
